b4488e4b42e1f292b4174346f7ea15c568109db1
Anushka Parajuli <parajulianushka1213@gmail.com>
2020-10-20 10:56:35 +0545

    Add files via upload

---
+++ MedicalArchive/passcodee.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Passcode page</title>
+</head>
+<body >
+
+	<center><h2>Passcode please!</h2>
+	<img src="img_avatar2.png" height="5%" width="10%">
+	<form>
+	<label for="pwd">Password:</label><br>
+  <input type="password" id="pwd" value="anushka">
+</form>
+  </center>
+
+</body>
+</html>
