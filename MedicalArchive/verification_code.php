8de2446a099a3f434c10da3292ac027eeb59056f
Anushka Parajuli <parajulianushka1213@gmail.com>
2020-10-30 15:08:59 +0545

    Add files via upload

---
+++ MedicalArchive/verification_code.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Verfication code ko page</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+	<h4>Please enter the 4-digit verification code which has been sent to email.</h4>
+	<form>
+  <label for="pin">Verification code :</label>
+  <input type="number" name="pin" id="pin" size="4">
+  <button type="submit"> Submit </button>
+</form>
+</body>
+</html>
